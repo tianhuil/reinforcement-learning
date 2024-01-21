f9773e9a36cfb4a7ac2c8fdff61a8ccb7964bb8e
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2024-01-20 23:33:37 -0500

    Fix test

--- src/test_logistics.py
+++ src/test_logistics.py
@@ -1,15 +1,15 @@
-from src.logistics import Logistics
+from src.logistics import Unloading, move_palette
 
 
 def test_transfer():
-    assert Logistics.transfer(1, 0) == (0, 1, True)
-    assert Logistics.transfer(0, 0) == (0, 0, False)
-    assert Logistics.transfer(0, 1) == (0, 1, False)
-    assert Logistics.transfer(1, 1) == (1, 1, False)
+    assert move_palette(1, 0) == (0, 1, True)
+    assert move_palette(0, 0) == (0, 0, False)
+    assert move_palette(0, 1) == (0, 1, False)
+    assert move_palette(1, 1) == (1, 1, False)
 
 
 def test_unload():
-    assert Logistics.unload(1, 0) == (1, 0, False)
-    assert Logistics.unload(0, 0) == (0, 0, False)
-    assert Logistics.unload(0, 1) == (0, 1, False)
-    assert Logistics.unload(1, 1) == (0, 0, True)
+    assert Unloading.unload(1, 0) == (1, 0, False)
+    assert Unloading.unload(0, 0) == (0, 0, False)
+    assert Unloading.unload(0, 1) == (0, 1, False)
+    assert Unloading.unload(1, 1) == (0, 0, True)
