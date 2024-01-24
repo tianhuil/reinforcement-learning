fec826d67dd01bc0efa5fcab6fafceffb527856a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2024-01-24 16:38:09 -0500

    Save model using id_str

--- src/train_logistics.py
+++ src/train_logistics.py
@@ -1,10 +1,14 @@
 import time
 
+from random_word import RandomWords
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.monitor import Monitor
 
 from src.logistics import Logistics
+
+r = RandomWords()
+id_str = "{}-{}".format(r.get_random_word(), r.get_random_word())
 
 env = Logistics(
     n_rows=3,
@@ -18,6 +22,7 @@ check_env(env)
 log_dir = "/tmp/gym/{}".format(int(time.time()))
 env = Monitor(env, log_dir, allow_early_resets=True)
 
-model = A2C("MultiInputPolicy", env, verbose=1, tensorboard_log="./data/tb/").learn(
-    200_000
-)
+model = A2C(
+    "MultiInputPolicy", env, verbose=1, tensorboard_log=f"./data/tb/{id_str}"
+).learn(200_000)
+model.save(f"./data/model/{id_str}")
