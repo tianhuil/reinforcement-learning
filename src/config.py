ace2b3297b85a4cdf84b466a8bd1df6c63f1edef
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2024-01-25 23:10:30 -0500

    Increase rate and speed up arrivals

--- src/config.py
+++ src/config.py
@@ -8,9 +8,9 @@ def new_logistics():
         n_rows=3,
         n_cols=2,
         palette_types=2,
-        prob_loading=0.1,
-        prob_unloading=0.2,
-        n_steps=50,
+        prob_loading=0.2,
+        prob_unloading=0.3,
+        n_steps=30,
     )
 
 
