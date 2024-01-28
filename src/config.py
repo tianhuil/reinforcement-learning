d167bed7dff1ef0b9582e23819d3e78958dbff1a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2024-01-28 12:18:17 -0500

    Save config

--- src/config.py
+++ src/config.py
@@ -10,8 +10,8 @@ def new_logistics():
     return Logistics(
         n_rows=4,
         n_cols=4,
-        palette_types=2,
-        prob_loading=0.2,
+        palette_types=4,
+        prob_loading=0.15,
         prob_unloading=0.3,
         n_steps=50,
         early_termination=False,
